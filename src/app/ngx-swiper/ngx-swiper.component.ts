ef8767031c8177d61c423525fd54b38b44127693
Matthew Vaughan <m0v0nage@gmail.com>
2017-04-26 16:41:17 -0600

    Working even better

--- src/app/ngx-swiper/ngx-swiper.component.ts
+++ src/app/ngx-swiper/ngx-swiper.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ContentChild, ViewChildren, ViewChild, QueryList, ElementRef, TemplateRef, Input } from '@angular/core';
+import { Component, OnInit, ContentChild, ViewChildren, ViewChild, QueryList, ElementRef, TemplateRef, Input, Output, EventEmitter } from '@angular/core';
 import { SwipeableDirective } from '../swipeable.directive';
 import $ from 'jquery';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'ngx-swiper',
@@ -15,36 +16,76 @@ export class NgxSwiperComponent implements OnInit {
   @ViewChild("scroller") scroller: ElementRef;
 
   @Input() swipeableData: any[];
+  @Input() move: Observable<any>;
+
+  @Output() newCentered: EventEmitter<any> = new EventEmitter<any>();
+
+  private centered: HTMLElement;
 
   constructor() { }
 
   ngOnInit() {
+    if (this.move) {
+      this.move.subscribe(m => {
+        console.log(m);
+        if(m.direction == 'forward') {
+          this.forwards(m.count);
+        } else if (m.direction == 'back') {
+          this.backwards(m.count);
+        }
+      });
+    }
   }
 
-  ngAfterContentInit() {
-    console.log(this.swipeableTemplate);
+  forwards(count) {
+    let list = this.swipeableItems.toArray().map(si => si.nativeElement);
+    let index = list.indexOf(this.centered);
+    index = index + count >= list.length ? list.length - 1 : index + count;
+    let element = list[index];
+    this.centerItem(element);
+  }
+
+  backwards(count) {
+    let list = this.swipeableItems.toArray().map(si => si.nativeElement);
+    let index = list.indexOf(this.centered);
+    index = index - count < 0 ? 0 : index - count;
+    let element = list[index];
+    this.centerItem(element);
   }
 
   ngAfterViewInit() {
-    console.log(this.swipeableItems);
+    //this.centerItem(this.swipeableItems.first.nativeElement);
   }
 
-  centerItem(event) {
-    let target = event.currentTarget;
-    let targetWidth = target.offsetWidth;
+  centerItemByIndex(index: number) {
+    this.centerItem(this.swipeableItems.toArray()[index].nativeElement);
+  }
+
+  centerItem(element: HTMLElement) {
+    let targetWidth = element.offsetWidth;
     let scrollerWidth = this.scroller.nativeElement.offsetWidth;
-    let pixelsToAnimate = target.offsetLeft + targetWidth/2 - scrollerWidth/2;
+    let pixelsToAnimate = element.offsetLeft + targetWidth/2 - scrollerWidth/2;
 
-	if(Math.abs(pixelsToAnimate) < 2)
-	  return;
+    if(Math.abs(pixelsToAnimate) < 2)
+      return;
 
-	let animated = false;
-	this.swipeableItems.forEach(si => {
-	  animated = $(si.nativeElement).is(':animated');
-	});
+    let animated = false;
+    this.swipeableItems.forEach(si => {
+      animated = $(si.nativeElement).is(':animated');
+    });
 
-	if (animated)
-	  return;
+    if (animated)
+      return;
+
+    this.centered = element;
+    let index = this.swipeableItems.toArray().map(si => si.nativeElement).indexOf(element);
+    this.newCentered.emit({ index: index, data: this.swipeableData[index]});
+
+    this.swipeableData.forEach(sd => {
+      sd['centered'] = false;
+    });
+
+    this.swipeableData[index]['centered'] = true;
 
     this.swipeableItems.map(si => si.nativeElement).forEach(ne => {
       $(ne).animate({ left: "-=" + pixelsToAnimate + "px" });
